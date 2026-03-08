f77cf4472c38f44816409fdf605607b76ea67425
Justin Fogo <jfogo@ucsc.edu>
2026-03-07 16:23:17 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// game configuration
+const config = {
+    parent: 'game',
+    type: Phaser.AUTO,
+    height: 600,
+    width: 800,
+    scene: [],
+}
+
+// define game
+let game = new Phaser.Game(config)
+
+const w = game.config.width
+const h = game.config.height
+
+console.log(w, h)
